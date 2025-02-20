6cc084901adfc7234d3caa298fd6220d45bc6bf3
Lukasz Gendek <lukasz.gendek@gmail.com>
2025-02-20 10:00:14 +0100

    update score

--- src/main/java/com/tramo/Service.java
+++ src/main/java/com/tramo/Service.java
@@ -4,4 +4,8 @@ public class Service {
     public void startMatch(String hometeam, String awayteam) {
         
     }
+
+    public void updateScore(String hometeam, int homeScore, String awayTeam, int awayScore) {
+        
+    }
 }
